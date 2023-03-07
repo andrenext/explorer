6bfd351542f839c3e99c4022a184190caa0f43b8
Andre Braga <contato.andredevbr@gmail.com>
2023-03-07 01:55:01 -0300

    feat!: pomodoro

--- Stage 09 - Frontend/rocketnotes/src/components/Header/index.jsx
+++ Stage 09 - Frontend/rocketnotes/src/components/Header/index.jsx
@@ -1,21 +1,24 @@
 import { RiShutDownLine } from 'react-icons/ri';
-import {Container, Profile, Logout} from "./styles.js";
+import { Container, Profile, Logout } from "./styles.js";
 
 export function Header() {
+  const macaco = "macaco";
+  console.log(macaco);
+
   return (
     <Container>
       <Profile>
-          <img src="https://github.com/luizf-lf.png" alt="Luiz Fernando" />
+        <img src="https://github.com/luizf-lf.png" alt="Luiz Fernando" />
 
-            <div>
-                <span>Bem-Vindo</span>
-                <strong>Luiz Fernando</strong>
-            </div>
+        <div>
+          <span>Bem-Vindo</span>
+          <strong>Luiz Fernando</strong>
+        </div>
       </Profile>
 
-        <Logout>
-            <RiShutDownLine />
-        </Logout>
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
   );
 }
